acf6b4eebe873c42487cf8fdc8be42b273a8ca8b
Rocela Durazo <roxdurazo@gmail.com>
2025-10-10 22:53:45 -0700

    chore: update dependencies in package.json
    - upgraded expo from ~54.0.12 to ~54.0.13
    - upgraded expo-font from ~14.0.8 to ~14.0.9
    - upgraded expo-router from ~6.0.10 to ~6.0.12
    - downgraded @types/jest from ^30.0.0 to ^29.4.4
    - downgraded jest from ^30.2.0 to ^29.7.0
    - downgraded ts-jest from ^30.0.0 to ^29.4.4

--- jest-setup.js
+++ jest-setup.js
@@ -1,94 +1,84 @@
 // Jest setup for React Native Testing Library
 
-// Mock AsyncStorage
-const AsyncStorage = {
-  getItem: jest.fn(() => Promise.resolve(null)),
-  setItem: jest.fn(() => Promise.resolve()),
-  removeItem: jest.fn(() => Promise.resolve()),
-  clear: jest.fn(() => Promise.resolve()),
-};
+// // Mock Expo Router
+// jest.mock('expo-router', () => ({
+//   router: {
+//     push: jest.fn(),
+//     replace: jest.fn(),
+//     back: jest.fn(),
+//   },
+//   Link: ({ children, href, ...props }) => {
+//     return React.createElement('a', { href, ...props }, children);
+//   },
+//   useRouter: () => ({
+//     push: jest.fn(),
+//     replace: jest.fn(),
+//     back: jest.fn(),
+//   }),
+// }));
 
-require('@react-native-async-storage/async-storage/jest/async-storage-mock');
+// // Mock React Native components properly
+// const React = require('react');
 
-// Mock Expo Router
-jest.mock('expo-router', () => ({
-  router: {
-    push: jest.fn(),
-    replace: jest.fn(),
-    back: jest.fn(),
-  },
-  Link: ({ children, href, ...props }) => {
-    return React.createElement('a', { href, ...props }, children);
-  },
-  useRouter: () => ({
-    push: jest.fn(),
-    replace: jest.fn(),
-    back: jest.fn(),
-  }),
-}));
+// jest.mock('react-native', () => {
+//   const mockComponent = (name) => {
+//     const MockedComponent = React.forwardRef((props, ref) => {
+//       return React.createElement(name, { ...props, ref });
+//     });
+//     MockedComponent.displayName = `Mocked${name}`;
+//     return MockedComponent;
+//   };
 
-// Mock React Native components properly
-const React = require('react');
+//   const mockTextComponent = React.forwardRef((props, ref) => {
+//     return React.createElement('span', { ...props, ref }, props.children);
+//   });
+//   mockTextComponent.displayName = 'MockedText';
 
-jest.mock('react-native', () => {
-  const mockComponent = (name) => {
-    const MockedComponent = React.forwardRef((props, ref) => {
-      return React.createElement(name, { ...props, ref });
-    });
-    MockedComponent.displayName = `Mocked${name}`;
-    return MockedComponent;
-  };
+//   const mockViewComponent = React.forwardRef((props, ref) => {
+//     return React.createElement('div', { ...props, ref }, props.children);
+//   });
+//   mockViewComponent.displayName = 'MockedView';
 
-  const mockTextComponent = React.forwardRef((props, ref) => {
-    return React.createElement('span', { ...props, ref }, props.children);
-  });
-  mockTextComponent.displayName = 'MockedText';
+//   const mockTextInputComponent = React.forwardRef((props, ref) => {
+//     return React.createElement('input', {
+//       ...props,
+//       ref,
+//       type: props.secureTextEntry ? 'password' : 'text',
+//       onChange: (e) => props.onChangeText && props.onChangeText(e.target.value),
+//     });
+//   });
+//   mockTextInputComponent.displayName = 'MockedTextInput';
 
-  const mockViewComponent = React.forwardRef((props, ref) => {
-    return React.createElement('div', { ...props, ref }, props.children);
-  });
-  mockViewComponent.displayName = 'MockedView';
+//   const mockTouchableOpacityComponent = React.forwardRef((props, ref) => {
+//     return React.createElement('button', {
+//       ...props,
+//       ref,
+//       onClick: props.onPress,
+//     }, props.children);
+//   });
+//   mockTouchableOpacityComponent.displayName = 'MockedTouchableOpacity';
 
-  const mockTextInputComponent = React.forwardRef((props, ref) => {
-    return React.createElement('input', {
-      ...props,
-      ref,
-      type: props.secureTextEntry ? 'password' : 'text',
-      onChange: (e) => props.onChangeText && props.onChangeText(e.target.value),
-    });
-  });
-  mockTextInputComponent.displayName = 'MockedTextInput';
-
-  const mockTouchableOpacityComponent = React.forwardRef((props, ref) => {
-    return React.createElement('button', {
-      ...props,
-      ref,
-      onClick: props.onPress,
-    }, props.children);
-  });
-  mockTouchableOpacityComponent.displayName = 'MockedTouchableOpacity';
-
-  return {
-    View: mockViewComponent,
-    Text: mockTextComponent,
-    TextInput: mockTextInputComponent,
-    TouchableOpacity: mockTouchableOpacityComponent,
-    ScrollView: mockComponent('div'),
-    StyleSheet: {
-      create: (styles) => styles,
-    },
-    Alert: {
-      alert: jest.fn(),
-    },
-    Platform: {
-      OS: 'ios',
-      select: jest.fn((obj) => obj.ios),
-    },
-    Dimensions: {
-      get: jest.fn(() => ({ width: 375, height: 667 })),
-    }
-  };
-});
+//   return {
+//     View: mockViewComponent,
+//     Text: mockTextComponent,
+//     TextInput: mockTextInputComponent,
+//     TouchableOpacity: mockTouchableOpacityComponent,
+//     ScrollView: mockComponent('div'),
+//     StyleSheet: {
+//       create: (styles) => styles,
+//     },
+//     Alert: {
+//       alert: jest.fn(),
+//     },
+//     Platform: {
+//       OS: 'ios',
+//       select: jest.fn((obj) => obj.ios),
+//     },
+//     Dimensions: {
+//       get: jest.fn(() => ({ width: 375, height: 667 })),
+//     }
+//   };
+// });
 
 // Mock Expo Router
 jest.mock('expo-router', () => ({
